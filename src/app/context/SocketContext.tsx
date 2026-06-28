5b08605189b6db1024a4eb185e36c2e51af8ef20
FLOYD <j.shaymanov786@gmail.com>
2026-06-28 20:46:37 +0900

    feat: prepare for production

--- src/app/context/SocketContext.tsx
+++ src/app/context/SocketContext.tsx
@@ -3,12 +3,15 @@ import { io, Socket } from "socket.io-client";
 
 const socket = io(process.env.REACT_APP_API_URL as string, {
   withCredentials: true,
-});
-export const SocketContext = createContext<Socket>(socket);
+}); //withCredentials: true degani — request bilan birga cookie / session / auth credentials ham yuborilishi mumkin.
+
+export const SocketContext = createContext<Socket>(socket); //React Context yaratamiz.
+//const socket = useContext(SocketContext); wunday qilib ishlatamiz bowqa componentlarda.
 
 interface SocketProviderProps {
   children: React.ReactNode;
-}
+} //SocketProvider ichida keladigan componentlar uchun type.
+
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
